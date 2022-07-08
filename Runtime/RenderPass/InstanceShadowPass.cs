211f0626663dac58feca6be978c838f3c3101d75
zhenyuhe <zhenyu.he@coconutislandstudio.com>
2022-07-08 19:00:53 +0800

    提交shadowcaster pass

--- Runtime/RenderPass/InstanceShadowPass.cs
+++ Runtime/RenderPass/InstanceShadowPass.cs
@@ -8,9 +8,18 @@ namespace Instances
 {
     public class InstanceShadowPass : ScriptableRenderPass
     {
+        const int k_ShadowmapBufferBits = 16;
+        private RenderTargetIdentifier source { get; set; }
+        private RenderTargetHandle destination { get; set; }
         public InstanceShadowPass()
         {
             renderPassEvent = RenderPassEvent.AfterRenderingShadows;
+        }
+        public void SetUp(RenderTargetIdentifier source, RenderTargetHandle destination)
+        {
+            
+            this.source = source;
+            this.destination = destination;
         }
         public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
         {
@@ -18,9 +27,20 @@ namespace Instances
         }
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
+            var m_ShadowCasterCascadesCount = renderingData.shadowData.mainLightShadowCascadesCount;
+            var renderTargetWidth = renderingData.shadowData.mainLightShadowmapWidth;
+            var renderTargetHeight = (m_ShadowCasterCascadesCount == 2) ?
+                renderingData.shadowData.mainLightShadowmapHeight >> 1 :
+                renderingData.shadowData.mainLightShadowmapHeight;
 
+            RenderTargetBinding rtb = new RenderTargetBinding();
             CommandBuffer cmd = CommandBufferPool.Get();
+            ShadowUtils.GetTemporaryShadowTexture(renderTargetWidth, renderTargetHeight, k_ShadowmapBufferBits);
+            //cmd.SetRenderTarget()
+            //RenderTargetIdentifier shadowmaskRT = ;
+
             
+            //RenderTexture rt = RenderTexture.GetTemporary();
 
 
             VisibleLight shadowlight = renderingData.lightData.visibleLights[renderingData.lightData.mainLightIndex];
